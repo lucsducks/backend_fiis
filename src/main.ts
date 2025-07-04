e8b26eb6c3e7793f6ecf1bfae152271c068b1460
Lucsducks <lucas1617y@gmail.com>
2025-07-04 12:52:47 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
 
-  // Carpeta 'uploads' accesible públicamente en /uploads/...
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
